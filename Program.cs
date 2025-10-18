945196f1261cd4af5be149d5bb06cb17c40918a2
ashkan.dehnavi <ashkan.dehnavi@dotin.ir>
2025-10-18 13:28:09 +0330

    fix

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Enter 2 numbers...");
+
+while (true)
+{
+    try
+    {
+        Console.WriteLine("enter first number: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("enter second number: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Sum: {a + b}");
+        Console.WriteLine($"========================");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("enter valid number!");
+    }
+}
+
+
+
